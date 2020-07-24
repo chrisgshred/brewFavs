fa95b1b39fea61e7de2e9c85026f8479846a5b1c
tober65 <timothy.ober@gmail.com>
2020-07-24 14:07:08 -0700

    Added bad search for beers and breweries

--- routes/api-beer-routes.js
+++ routes/api-beer-routes.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+sequelize = require("sequelize");
 
 module.exports = function (app) {
     app.post("/api/beer", (req, res) => {
@@ -16,18 +17,51 @@ module.exports = function (app) {
     });
 
     app.get("/api/beer", (req, res) => {
-        let lookupValue = req.body.name.toLowerCase();
+        let lookupValue;
 
-        db.Beer.findAll({
-            limit: 10,
-            where: {
-                name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + lookupValue + '%')
-            }
-        }).then((dbPost) => {
-            console.log(dbPost);
-            res.redirect(307, "/profile");
-        }).catch(err => {
-            res.status(401).json(err);
-        });
+        if (req.body.name && req.body.name !== "") {
+            lookupValue = req.body.name.toLowerCase();
+
+            db.Beer.findAll({
+                limit: 10,
+                where: {
+                    name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        }
+        else if (req.body.style && req.body.style !== "") {
+            lookupValue = req.body.style.toLowerCase();
+
+            db.Beer.findAll({
+                limit: 10,
+                where: {
+                    style: sequelize.where(sequelize.fn('LOWER', sequelize.col('style')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        } else if (req.body.ounces && req.body.ounces !== "") {
+            lookupValue = req.body.ounces.toLowerCase();
+
+            db.Beer.findAll({
+                limit: 10,
+                where: {
+                    ounces: sequelize.where(sequelize.fn('LOWER', sequelize.col('ounces')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        }
     });
 }
--- routes/api-brewery-routes.js
+++ routes/api-brewery-routes.js
@@ -14,18 +14,52 @@ module.exports = function (app) {
     });
 
     app.get("/api/brewery", (req, res) => {
-        let lookupValue = req.body.name.toLowerCase();
+        let lookupValue;
 
-        db.Brewery.findAll({
-            limit: 10,
-            where: {
-                name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + lookupValue + '%')
-            }
-        }).then((dbPost) => {
-            console.log(dbPost);
-            res.redirect(307, "/profile");
-        }).catch(err => {
-            res.status(401).json(err);
-        });
+        if (req.body.name && req.body.name !== "") {
+            lookupValue = req.body.name.toLowerCase();
+
+            db.Brewery.findAll({
+                limit: 10,
+                where: {
+                    name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        }
+        else if (req.body.city && req.body.city !== "") {
+            lookupValue = req.body.city.toLowerCase();
+            
+            db.Brewery.findAll({
+                limit: 10,
+                where: {
+                    city: sequelize.where(sequelize.fn('LOWER', sequelize.col('city')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        }
+        else if (req.body.state && req.body.state !== "") {
+            lookupValue = req.body.state.toLowerCase();
+            
+            db.Brewery.findAll({
+                limit: 10,
+                where: {
+                    state: sequelize.where(sequelize.fn('LOWER', sequelize.col('state')), 'LIKE', '%' + lookupValue + '%')
+                }
+            }).then((dbPost) => {
+                console.log(dbPost);
+                res.json(dbPost);
+            }).catch(err => {
+                res.status(401).json(err);
+            });
+        }
     });
 }
